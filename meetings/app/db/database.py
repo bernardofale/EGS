ad3d087b89f63f481ef7507f75ff619853809d25
Bernardo Falé <mbfale@ua.pt>
2024-05-13 18:10:08 +0100

    Changed HelloSign API key due to security reasos; Added DB and service to project's docker compose

--- meetings/app/db/database.py
+++ meetings/app/db/database.py
@@ -8,6 +8,6 @@ PG_USER = os.getenv("PG_USER")
 PG_PASSWORD = os.getenv("PG_PASSWORD")
 
 SQLALCHEMY_DATABASE_URL = "postgresql://" + PG_USER + ":" + PG_PASSWORD + \
-                            "@db:5432/postgres"
+                            "@meetings_db:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
