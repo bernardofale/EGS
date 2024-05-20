be228f75d5485afca8119d4f1fac270faea44ab1
Bernardo Falé <mbfale@ua.pt>
2024-05-20 22:34:37 +0100

    hardcoded api keys

--- meetings/app/dependencies.py
+++ meetings/app/dependencies.py
@@ -1,11 +1,8 @@
 from fastapi import HTTPException, Depends
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-API_KEY = os.getenv("API_KEY")
-HELLOSIGN_API_KEY = os.getenv("HELLOSIGN_KEY")
+API_KEY = 'o3e17a9d25945d0d7d009ccd62a5a7816265d3c6ffa03334a85cfd74be10c55e7'
+HELLOSIGN_API_KEY = 'o878dfbff49eceb3431c5b405b8e318117418efd536c9f0a0cd8881ed13b305db'
 security = HTTPBearer()
 
 
